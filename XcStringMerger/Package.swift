54d68a57d760ec3bfc9a69370a03b2f2ce146e73
pan93412 <pan93412@gmail.com>
2024-02-18 16:44:39 +0800

    Reformat the code

--- XcStringMerger/Package.swift
+++ XcStringMerger/Package.swift
@@ -20,8 +20,7 @@ let package = Package(
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
-    .target(
-      name: "XcStringMerger"),
+    .target(name: "XcStringMerger"),
     .testTarget(
       name: "XcStringMergerTests",
       dependencies: ["XcStringMerger"]
