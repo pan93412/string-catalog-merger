0e2483b77f9eea3570b94eaa59d47de1ba25e2e9
ShikiSuen <shikisuen@outlook.com>
2024-02-18 17:29:19 +0800

    License headers and CoC.

--- XcStringMerger/Tests/XcStringMergerTests/MergerTest.swift
+++ XcStringMerger/Tests/XcStringMergerTests/MergerTest.swift
@@ -1,3 +1,8 @@
+// (c) 2024 and onwards The SCMerger Project (AGPL-3.0 License).
+// ====================
+// This code is released under the AGPL v3.0 license (SPDX-License-Identifier: AGPL-3.0)
+// SCMerger is short for "String Catalog Merger", a project founded by Pan93412.
+
 @testable import XcStringMerger
 import XCTest
 
